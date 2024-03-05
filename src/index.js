a2c77641abd9ef9a80f7e3c27eff2b9ae335f4c2
Safal kafle <safalkafle12345@gmail.com>
2024-03-05 20:58:46 +0100

    git

---
+++ src/index.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBVTRxGIiwCqPqiNt5PvfRnjc9ujD9OKPs",
+    authDomain: "fire-base-9--10.firebaseapp.com",
+    projectId: "fire-base-9--10",
+    storageBucket: "fire-base-9--10.appspot.com",
+    messagingSenderId: "732304743261",
+    appId: "1:732304743261:web:f6673e1c0a2567c08a66b4"
+  };
