1cd87b5f1eaee1c9e0726ac0ea0740dc83aeb84f
Safal kafle <safalkafle12345@gmail.com>
2024-03-07 13:38:01 +0100

    config

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
+import {initializeApp} from 'firebase/app'
+import {getFireStore, collection} from 'firebase/firestore'
+
+
 const firebaseConfig = {
     apiKey: "AIzaSyBVTRxGIiwCqPqiNt5PvfRnjc9ujD9OKPs",
     authDomain: "fire-base-9--10.firebaseapp.com",
@@ -6,3 +10,8 @@ const firebaseConfig = {
     messagingSenderId: "732304743261",
     appId: "1:732304743261:web:f6673e1c0a2567c08a66b4"
   };
+
+initializeApp(firebaseConfig)
+let db = getFireStore();
+
+const colRef = collection(db, 'books')
